92c2e00c22bb14f52006166de2e167b61460e817
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-27 03:41:14 +0300

    Created a routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'cheeses', to: 'cheeses#index'
 end
+
+
+
+#index method will always be called when user goes to /cheeses
+# 'cheeses#index' tells the Rails routing system
+#  that this route should be passed through 
+#  the CheesesController's index action.
