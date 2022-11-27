8e2facbf19e4c23b4a7677c557de2f2b12e832a7
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-27 04:01:26 +0300

    Routing

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,2 +1,10 @@
+# The standard naming convention for controllers 
+# is the name of the controller followed by the 
+# word Controller.
+
 class CheesesController < ApplicationController
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+      end
 end
